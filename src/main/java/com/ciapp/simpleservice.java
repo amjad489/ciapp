ddf0173752bcc32ac80a736c75d2869d60e1fcb7
Amjad <amjad.syed@icloud.com>
2017-01-29 19:48:28 +0530

    -added command line arguments

--- src/main/java/com/ciapp/simpleservice.java
+++ src/main/java/com/ciapp/simpleservice.java
@@ -1,11 +1,47 @@
 package com.ciapp;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class simpleservice {
 
 	public static void main(String[] args) {
-		System.out.println("Maven app");
-		// TODO Auto-generated method stub
+		Options options = new Options();
+		options.addOption("n", true, "[name] your name");
+		Option timeOption = new Option("t", false, "current time");
+		options.addOption(timeOption);
+		CommandLineParser parser = new BasicParser();
+		CommandLine cmd;
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException pe) {
+			usage(options);
+			return;
+		}
+		if (cmd.getOptions().length == 0){
+			usage(options);
+			System.exit(1);
+			
+		}
+		if (cmd.hasOption("t")) {
 
+			System.err.println("Date/Time: " + new java.util.Date());
+		}
+		if (cmd.hasOption("n")) {
+			System.err.println("Nice to meet you: " + cmd.getOptionValue('n'));
+		}
+	}
+
+	private static void usage(Options options) {
+
+		// Use the inbuilt formatter class
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("CLIDemo", options);
 	}
 
 }
